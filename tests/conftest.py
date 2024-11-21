0b390feb02d5c1fc1d4fa2043684a33ff9583542
Denis Kvartych <Denis.Kvartych@yandex.ru>
2024-11-21 10:18:48 +0300

    Убрал из фикстуры try и finally.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,8 @@ from selenium import webdriver
 @pytest.fixture(scope="function") # фикстура, которая создает экземпляр webdriver
 def driver():
     driver = webdriver.Chrome()
-    try:
-        yield driver
-    finally:
-        driver.quit()
+    yield driver
+    driver.quit()
 
 @pytest.fixture # фикстура, которая создает рандомную почту для регистрации
 def generate_login():
